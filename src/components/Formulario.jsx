15ddeeaa910a5ac1f294db5e854afad10fe5290c
juanfed <juanfed12345@gmail.com>
2022-03-03 08:36:17 -0500

    antes de usar useContex

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import '../styles/formulario.css'
 
 const Formulario = () => {
     return (
-        <div>Formulario</div>
+        <form className='formulario'>
+
+            <div className='input div'  >
+                <input type="text"
+                placeholder='busca tu bebida'
+                required
+                name='nombre' />
+            </div>
+
+            <div className='input2 div'>
+                <select htmlFor="categoria">
+                    <option value="">- Selecciona el tipo de bebida -</option>
+                </select>
+            </div>
+            <div className='buton'>
+                <button type='submit'>Buscar bebida</button>
+            </div>
+        </form>
     )
 }
 
